d6e8e3cc35b73677923ad50ac1763a0e8825361a
Sanjaee <afrizaahmad18@gmail.com>
2025-11-21 21:12:38 +0700

    Update ESLint configuration to disable the no-explicit-any rule and add new Radix UI components to dependencies in package.json and package-lock.json.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    // Disable no-explicit-any rule to allow usage of 'any' type
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
